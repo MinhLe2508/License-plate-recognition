db1ddf4b7eb96336850c14fe2c14bb18a4491e40
MinhLe2508 <lnminh1234567@gmail.com>
2026-06-03 21:59:21 +0700

    Thêm order_points.py và get_plates_corners

--- preprocessing.py
+++ preprocessing.py
@@ -35,3 +35,44 @@ def detect_license_plates(model: YOLO, image: np.ndarray) -> list[dict]:
     detections.sort(key=lambda d: d["conf"], reverse=True)
     return detections
 
+def order_points(pts: np.ndarray) -> np.ndarray:
+    rect    = np.zeros((4, 2), dtype=np.float32)
+    s       = pts.sum(axis=1)
+    diff    = np.diff(pts, axis=1)
+    
+    rect[0] = pts[np.argmin(s)]     # (Top-Left)        x + y nhỏ nhất
+    rect[1] = pts[np.argmax(s)]     # (Bottom-Right)    x + y lớn nhất
+    rect[2] = pts[np.argmin(diff)]  # (Top-Right)       y - x nhỏ nhất
+    rect[3] = pts[np.argmax(diff)]  # (Bottom-Left)     y - x lớn nhất
+    return rect
+
+def get_plate_corners(detection: dict, img_shape: tuple) -> np.ndarray:
+    h, w = img_shape[:2]
+    
+    if detection["polygon"] is not None and len(detection["polygon"]) >= 4:
+        polygon = detection["polygon"]
+        
+        hull = cv2.convexHull(polygon)
+        
+        epsilon = 0.02 * cv2.arcLength(hull, True)
+        approx  = cv2.approxPolyDP(hull, epsilon, True)
+        
+        if len(approx) == 4:
+            corners = approx.reshape(4, 2).astype(np.float32)
+        else:
+            x, y, bw, bh = cv2.boundingRect(polygon)
+            corners = np.array([
+                [x,      y     ],
+                [x + bw, y     ],
+                [x + bw, y + bh],
+                [x,      y + bh],
+            ], dtype=np.float32)
+            
+    else:
+        x1, y1, x2, y2 = detection["bbox"]
+        corners = np.array([
+            [x1, y1], [x2, y1],
+            [x2, y2], [x1, y2],
+        ], dtype=np.float32)
+        
+    return order_points(corners)   
